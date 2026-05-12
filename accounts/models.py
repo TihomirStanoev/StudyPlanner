2483f84a17f01c6c7e9dbd6e729abde3a970ec8d
tihomir <tstanoev@yandex.com>
2026-05-12 22:45:46 +0300

    accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
+from accounts.validators import MaxYearValidator
 
-# Create your models here.
+
+
+
+class CustomUser(AbstractUser):
+    birth_year = models.PositiveIntegerField(validators=[MinValueValidator(1900), MaxYearValidator()])
