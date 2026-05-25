532add27b4de59b5dc0c0b6b622c739eca15c985
tihomir <tstanoev@yandex.com>
2026-05-25 21:38:41 +0300

    migrations

--- topics/models.py
+++ topics/models.py
@@ -1,8 +1,7 @@
 from django.core.validators import RegexValidator, MinLengthValidator
 from django.db import models
 from django.conf import settings
-from common.models import SoftDeletionModel
-
+from common.models import SoftDeletionModel, BaseModel
 
 
 class Topic(SoftDeletionModel):
@@ -26,3 +25,26 @@ class Topic(SoftDeletionModel):
 
     def __str__(self):
         return f'{self.name[:30]}{"..." if len(self.name) > 30 else ""}'
+
+
+class Resource(BaseModel):
+    class TypeChoose(models.TextChoices):
+        VIDEO = 'video', 'Video'
+        ARTICLE = 'article', 'Article'
+        BOOK = 'book', 'Book'
+        OTHER = 'other', 'Other'
+
+    class StatusChoose(models.IntegerChoices):
+        NOT_STARTED = 0, 'Not Started'
+        IN_PROGRESS = 1, 'In Progress'
+        DONE = 2, 'Done'
+
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    type = models.CharField(max_length=30, choices=TypeChoose, default=TypeChoose.OTHER)
+    status = models.SmallIntegerField(choices=StatusChoose, default=StatusChoose.NOT_STARTED)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='resources')
+
+
+    def __str__(self):
+        return f'{self.title} - {self.status}'
