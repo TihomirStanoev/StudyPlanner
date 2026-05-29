c5c290bff30831df6ad096812cbfe40b34fca2eb
tihomir <tstanoev@yandex.com>
2026-05-29 06:26:50 +0300

    admin setup

--- topics/admin.py
+++ topics/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from topics.models import Resource, Topic
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'type', 'status', 'topic')
+    list_filter = ('type', 'status')
+    search_fields = ('title', 'url')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'color', 'owner')
+    list_filter = ('owner',)
+    search_fields = ('name', 'description')
+    ordering = ('-created_at',)
--- tracking/admin.py
+++ tracking/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from tracking.models import Goal, StudySession
+
+
 # Register your models here.
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'target_minutes', 'deadline', 'is_completed')
+    list_filter = ('is_completed',)
+    search_fields = ('topic__name',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(StudySession)
+class StudySessionAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'date', 'duration_minutes', 'notes')
+    list_filter = ('date',)
+    search_fields = ('topic__name',)
+    ordering = ('-created_at',)
